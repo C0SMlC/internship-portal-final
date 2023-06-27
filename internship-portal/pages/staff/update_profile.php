b7d0cec38896f248c26b9c0784d26d3dd530653a
Pratik <pendurkarpratik04@gmail.com>
2023-06-27 21:40:16 +0530

    Redirecting user back to home screen after data updation

--- internship-portal/pages/staff/update_profile.php
+++ internship-portal/pages/staff/update_profile.php
@@ -9,10 +9,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['fullName']) && isset($
     $mobile = $_POST['mobile'];
     $address = $_POST['address'];
 
-    if (update_existing_data($con, $fullName, $email, $age, $mobile, $address)) {
-        echo "Data updated successfully.";
+   if (update_existing_data($con, $fullName, $email, $age, $mobile, $address)) {
+    echo "Data updated successfully.";
+    echo "<a onclick=\"goToHomeScreen()\" style=\"cursor: pointer; color: blue;\">Go back to Home Screen</a>";
     } else {
-        echo "Failed to update data.";
+    echo "Failed to update data.";
     }
+
+    echo "<script>
+    function goToHomeScreen() {
+        window.location.href = 'http://localhost/internship-portal-final/internship-portal/pages/student'; // Replace with your home screen URL
+    }
+    </script>";
+
 }
 ?>
--- internship-portal/pages/student/update_profile.php
+++ internship-portal/pages/student/update_profile.php
@@ -2,17 +2,17 @@
 require '../../components/student-profile/connect.php';
 
 // Check if $_POST data is available before calling update_data function
-if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['fullName']) && isset($_POST['email']) && isset($_POST['age']) && isset($_POST['mobile']) && isset($_POST['address'])) {
-    $fullName = $_POST['fullName'];
-    $email = $_POST['email'];
-    $age = $_POST['age'];
-    $mobile = $_POST['mobile'];
-    $address = $_POST['address'];
-
-    if (update_existing_data($con, $fullName, $email, $age, $mobile, $address)) {
-        echo "Data updated successfully.";
-    } else {
-        echo "Failed to update data.";
-    }
+if (update_existing_data($con, $fullName, $email, $age, $mobile, $address)) {
+    echo "Data updated successfully.";
+    echo "<a onclick=\"goToHomeScreen()\" style=\"cursor: pointer; color: blue;\">Go back to Home Screen</a>";
+} else {
+    echo "Failed to update data.";
 }
+
+echo "<script>
+    function goToHomeScreen() {
+        window.location.href = 'http://localhost/internship-portal-final/internship-portal/pages/student'; // Replace with your home screen URL
+    }
+</script>";
+
 ?>
