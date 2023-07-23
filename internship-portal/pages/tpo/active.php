28164993bcafcd17f48036142a104e0c351677bd
Prasad <prasadb21extc@student.mes.ac.in>
2023-07-24 00:02:20 +0530

    On branch parallel-branch  Your branch is up to date with 'origin/parallel-branch'.  Changes to be committed: 	modified:   internship-portal/pages/tpo/active.php

--- internship-portal/pages/tpo/active.php
+++ internship-portal/pages/tpo/active.php
@@ -78,7 +78,7 @@ $db_connection->close();
                 // Output row data
                 echo "<tr>";
                 echo "<td class='text-center'>" . $row["announcement_id"] . "</td>";
-                echo "<td class='text-center'><a href='./index copy.php?announcement_title=" . urlencode($row["announcement_title"]) . "' class='company-link'>" . $row["announcement_title"] . "</a></td>";
+                echo "<td class='text-center'><a href='./companyInfo.php?announcement_title=" . urlencode($row["announcement_title"]) . "' class='company-link'>" . $row["announcement_title"] . "</a></td>";
                 echo "<td class='text-center'>" . $row["start_date"] . "</td>";
                 echo "<td class='text-center'>" . $row["duration"] . "</td>";
                 echo "<td class='text-center'><a href='./students-for-company.php?announcement_title=" . urlencode($row["announcement_title"]) ."' class='btn btn-primary ' role='button'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-download' viewBox='0 0 16 16'><path d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z'/><path d='M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z'/></svg></a></td>";
