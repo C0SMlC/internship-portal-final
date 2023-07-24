3b0f39cf879ad10f2cabda7830a4c7bdc604b3cd
Jyoti Anand Chandra Prasad <jyotianandchandraprasad@gmail.com>
2023-07-24 15:49:19 +0530

    On branch parallel-branch  Your branch is up to date with 'origin/parallel-branch'.  Changes to be committed: 	modified:   internship-portal/components/announcement/index.php

--- internship-portal/components/announcement/index.php
+++ internship-portal/components/announcement/index.php
@@ -1,53 +1,56 @@
 <?php
-//require "../../connect/connect.php";
+// Replace with your database credentials
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "internship_portal";
 
-// $query = "select * from new_annoucement";
-// mysqli_query($db_connection, $query);
+// Create a connection to the database
+$db_connection = mysqli_connect($hostname, $username, $password, $database);
+
+// Check if the connection was successful
+if (mysqli_connect_errno()) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+
+// Query to fetch the values of attributes from the table
+$query = "SELECT announcement_id, announcement_title, branch FROM new_annoucement";
+
+// Execute the query
+$result = mysqli_query($db_connection, $query);
+
+// Check if the query was successful
+if ($result) {
+    // Fetch the data from the result set and store it in an array
+    $announcements = mysqli_fetch_all($result, MYSQLI_ASSOC);
+} else {
+    // Query execution failed
+    die("Error executing the query: " . mysqli_error($db_connection));
+}
+
+// Close the database connection
+mysqli_close($db_connection);
 ?>
+
 <div class="announcement">
     <div class="announcementTitle">
         <p>Important Announcements:-</p>
     </div>
     <div class="messageDisplay">
         <ol>
+            <?php foreach ($announcements as $announcement): ?>
             <li>
-                <b>
-                    &#x25CF;
-                </b>
-                <!-- <a href="../../pages/Internship/index.php?id=<?php echo $announcement_id; ?>">
-                    $announcement_title
-                    <strong>Click to view !</strong>
-                </a> -->
-
-                <a href="../../pages/Internship/index.php">
-                    XYZ PVT LTD requires interns for it's IT department.
-                    <strong>Click to view !</strong>
-                </a>
-
-            </li>
-            <li>
-                <b>
-                    &#x25CF;
-                </b>
-                <a href="../../pages/Internship/index.php">
-                    XYZ PVT LTD requires interns for it's IT department.
+                <b>&#x25CF;</b>
+                <a href="../../pages/Internship/index.php?id=<?php echo $announcement['announcement_id']; ?>">
+                    <?php echo $announcement['announcement_title']; ?> requires interns for its <?php echo $announcement['branch']; ?> branch
                     <strong>Click to view !</strong>
                 </a>
             </li>
-            <li>
-                <b>
-                    &#x25CF;
-                </b>
-                <a href="../../pages/Internship/index.php">
-                    XYZ PVT LTD requires interns for it's IT department.
-                    <strong>Click to view !</strong>
-                </a>
-            </li>
-
+            <?php endforeach; ?>
         </ol>
-
     </div>
 </div>
+
 <style>
     .messageDisplay {
         background-color: #f5f5f5;
